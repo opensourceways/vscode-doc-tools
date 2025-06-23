82d591fda92e6fa60d0bb489afa5675877f2594e
luckyasme <807254037@qq.com>
2025-06-23 11:20:24 +0800

    chore: 拆分多包

--- packages/extension/webpack.config.mjs
+++ packages/extension/webpack.config.mjs
@@ -9,7 +9,7 @@ const extensionConfig = {
 
   entry: './src/extension.ts',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../../dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
   },
