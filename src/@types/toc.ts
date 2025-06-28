94624120c34db6618b924dff9180a51211a34c15
luckyasme <807254037@qq.com>
2025-06-28 14:09:31 +0800

    fix: 优化 toc 相关检查

--- src/@types/toc.ts
+++ src/@types/toc.ts
@@ -1,14 +1,9 @@
 export interface TocItem {
   label: string; // 名称
+  isManual?: boolean; // 是否为手册
   href?: string | { // 链接
     upstream: string;
     path?: string;
   };
-  sections?: TocItem[]; // 子节点
-}
-
-export interface Toc {
-  label: string; // 名称
-  isManual?: boolean; // 是否为手册
   sections?: TocItem[]; // 子节点
 }
