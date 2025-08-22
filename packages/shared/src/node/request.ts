58b44211a3445daa6c82649b21611c9f942a5da6
luckyasme <807254037@qq.com>
2025-08-22 17:38:47 +0800

    fix: 修复gitee某些链接会误报404的问题

--- packages/shared/src/node/request.ts
+++ packages/shared/src/node/request.ts
@@ -29,6 +29,7 @@ export function createRequest(
     method,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     },
     signal: controller.signal,
   }).finally(() => {
